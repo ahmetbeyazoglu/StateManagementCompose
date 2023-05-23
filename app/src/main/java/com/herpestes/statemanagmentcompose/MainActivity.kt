46d1c0ea1afececa9623c2bc80bab0a7a7f7828d
herpestes <ahmetbeyazoglu@gmail.com>
2023-05-23 22:45:03 +0300

    Compose Image

--- app/src/main/java/com/herpestes/statemanagmentcompose/MainActivity.kt
+++ app/src/main/java/com/herpestes/statemanagmentcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.herpestes.statemanagmentcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -12,6 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.herpestes.statemanagmentcompose.ui.theme.StatemanagmentcomposeTheme
@@ -23,10 +30,12 @@ class MainActivity : ComponentActivity() {
             MainScreen()
         }
     }
+
+
 }
 
 @Composable
-fun MainScreen(name: String) {
+fun MainScreen() {
    Surface(color = Color.LightGray) {
        Column(modifier = Modifier.fillMaxSize(),
            verticalArrangement = Arrangement.Center,
@@ -35,14 +44,26 @@ fun MainScreen(name: String) {
            Text(text = "Hello!")
            
            Spacer(modifier = Modifier.padding(7.dp))
+
+           Image(bitmap = ImageBitmap.imageResource(id = R.drawable.kenny),
+           contentDescription = "Kenny")
            
+           Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background), contentDescription = null )
+
+           Spacer(modifier = Modifier.padding(7.dp))
+
+           Image(painter = ColorPainter(Color.Black), contentDescription = null)
+
+           /*
            Button(onClick = {
                println("Clicked")
            }) {
                Text(text = "button")
                Text(text = "Test")
 
-           }
+           }*/
 
        }
    }
